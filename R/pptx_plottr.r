249e3ea1e38b9bae724e163a8a2c7712853060bd
rpoulsen47 <ryan.poulsen47@gmail.com>
2026-07-22 14:44:12 -0600

    minor updates

--- R/pptx_plottr.r
+++ R/pptx_plottr.r
@@ -9,30 +9,28 @@
 # Import Libraries
 library(ggplot2)
 library(officer)
-library(magrittr)
 library(rvg)
 library(here)
 
 #'
 #' Powerpoint Ready Plot
 #' @description Powerpoint Ready Plots for Separate-and-Editable Figures Made Easy
-#' @param plt A ggplot or gridExtra plot object.
+#' @param plt A ggplot or R plot object.
 #' @param pptx_path Path to powerpoint presentation to export.
-#' @importFrom magrittr %>%
 #' @export
 #'
 pptx_plot <- function(plt, pptx_path) {
   if (ggplot2::is_ggplot(plt)) { # if plt is a ggplot object
-    plt_dml <- rvg::dml(plot(plt))
-  } else { # if plt is not a ggplot object, perhaps gridExtra
-    plt_dml <- rvg::dml(plot(plt))
+    plt_dml <- rvg::dml(ggobj = plt)
+  } else { # if plt is not a ggplot object, perhaps gridExtra or other plot
+    plt_dml <- rvg::dml(code = plt)
   }
   # initialize PowerPoint slide
-  officer::read_pptx() %>%
+  officer::read_pptx() |>
     # add slide
-    officer::add_slide(layout = "Blank") %>%
+    officer::add_slide(layout = "Blank") |>
     # add plot to slide at full-size
-    officer::ph_with(plt_dml, officer::ph_location_fullsize()) %>%
+    officer::ph_with(plt_dml, officer::ph_location_fullsize()) |>
     # export slide; if .pptx already exists, update file, otherwise create new .pptx
     print(target = here::here(pptx_path)
     )
